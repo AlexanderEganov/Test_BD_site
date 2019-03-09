c683fc105057ed5a70450c0f39e0a130b7dab26d
AlexanderEganov <48315596+AlexanderEganov@users.noreply.github.com>
2019-03-09 15:43:01 +0300

    Create urls.py

---
+++ my_site/book/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'book'
+
+urlpatterns = [
+    #url(r'^$', views.index, name='index'),
+    url(r'^$', views.loading, name='loading'),
+]
